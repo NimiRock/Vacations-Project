6d3c613d4dcc1f66cd2628555929a5f86659815e
Nimrod Rokach <nimrod.rock@gmail.com>
2020-12-25 21:55:25 +0200

    another commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
+const path = require('path')
 const port = process.env.PORT || 8080;
 
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use('/auth', require('./routes/auth&credentials'));
 app.use('/vacations', require('./routes/vacations'));
